096bd5ce644f151685b491e411a11233535a0560
Mayur Yadav <mayurmyadavarc@gmail.com>
2026-01-21 12:12:10 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,10 @@
+# Write a program to find the sum of first n natural numbers using while loop.
+
+n = int(input("entre the value of n : "))
+a = 1
+sum = 0
+while(a<=n):
+    sum += a
+    a=a+1
+print(sum)
+  
